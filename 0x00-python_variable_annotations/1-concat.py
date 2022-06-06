605225682d24c6edbc7c353375ff9e60772c11bf
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-06-06 17:18:00 -0500

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""1. Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes two strings as arguments and
+    returns a concatenated string
+
+    Parameters
+    ----------
+    str1 : str
+        string
+    str1 : str
+        string
+
+    Returns
+    -------
+    str
+        concatenated string
+    """
+    return f"{str1} {str2}"
