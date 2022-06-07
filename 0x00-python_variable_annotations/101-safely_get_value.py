7406b8dae076d6ab5258c1934daf68533033c0b5
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-06-06 21:35:31 -0500

    11. More involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""10. Duck typing - first element of a sequence"""
+
+
+from typing import Any, Dict, Mapping, Sequence, TypeVar, Union
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('T'), None] = None) -> Union[Any, TypeVar('T')]:
+    """Given the parameters and the return values
+
+    Parameters
+    ----------
+    dct : Mapping
+    key : Any
+    default: Union[TypeVar('T'), None] = None
+
+    Returns
+    -------
+    Mapping
+        Union[Any, TypeVar('T')]
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
