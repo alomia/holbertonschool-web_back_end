167c4644d1cc6edf8258bce677bad997e025926d
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-06-09 00:03:02 -0500

    1. Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,9 @@
 
 import asyncio
 from typing import List
-async_generator = __import__("async_generator").async_generator
+
+
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
