912485674604a930334f1a562ddfc42727d3597e
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-11-01 23:43:17 -0500

    Writing strings to Redis

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Cache Module"""
+
+import redis
+import uuid
+
+
+class Cache:
+    """This class represents a cache object in redis
+
+    Attributes:
+
+    """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str) -> str:
+        """This method takes a string as an argument, generates a random key,
+            stores the input data in Redis using the random key and returns the key.
+        Args:
+            data (str): [STRING]
+        """
+        key: str = str(uuid.uuid4())
+        r = redis.Redis(host="localhost", port=6379, db=0)
+        r.set(key, data)
+        return key
