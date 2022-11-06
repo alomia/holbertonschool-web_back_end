9d464853de9d2d73b0e7e98a21cc0c729db985f5
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-11-05 23:46:12 -0500

    In and not out

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -3,6 +3,6 @@
 CREATE TABLE IF NOT EXISTS `users` (
   `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL UNIQUE,
-  `name` VARCHAR(255)
-  `country`
+  `name` VARCHAR(255),
+  `country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
