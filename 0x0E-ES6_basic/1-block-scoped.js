0b8369688731e55849b74799a62917e46e8c87f0
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-11-10 19:03:23 -0500

    add new file

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,9 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    
+    task = task;
+    task2 = task2;
   }
 
   return [task, task2];
