8beb15b43bcb6f4c9569adf89eab4fed3e24ef2f
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-11-15 20:45:24 -0500

    Don't make a promise...if you know you can't keep it

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -5,10 +5,11 @@ const getFullResponseFromAPI = (success) => {
     if (success) {
       resolve({
         status: 200,
-        body: "Success",
+        body: 'Success',
       });
+    } else {
+      reject(new Error('The fake API is not working currently'));
     }
-    reject(new Error("The fake API is not working currently"));
   });
 };
 
