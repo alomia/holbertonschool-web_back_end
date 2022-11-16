0e6c839b186af13a520e5b30de43efa8bf371750
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-11-15 20:16:39 -0500

    Keep every promise you make and only make promises you can keep

--- 0x0F-ES6_promise/0-promise.js
+++ 0x0F-ES6_promise/0-promise.js
@@ -1,3 +1,5 @@
-function getResponseFromAPI() {
+// Keep every promise you make and only make promises you can keep
+
+export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => [resolve, reject]);
 }
