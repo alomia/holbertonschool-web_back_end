ed743f77543e42bb8095b4b0ca62125ab03bd9cd
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-11-17 22:02:29 -0500

    A Course, Getters, and Setters

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -10,28 +10,34 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
+  /* getter name */
   get name() {
     return this._name;
   }
 
+  /* setter name */
   set name(name) {
     if (typeof name !== 'string') throw TypeError('Name must be a string');
     this._name = name;
   }
 
+  /* getter length */
   get length() {
     return this._length;
   }
 
+  /* setter length */
   set length(length) {
     if (typeof length !== 'number') throw TypeError('Length must be a number');
     this._length = length;
   }
 
+  /* getter students */
   get students() {
     return this._students;
   }
 
+  /* setter students */
   set students(students) {
     if (!Array.isArray(students)) throw TypeError('Stundents must be an array of Strings');
     if (students.every((student) => typeof student !== 'string')) throw TypeError('Stundents must be an array of Strings');
