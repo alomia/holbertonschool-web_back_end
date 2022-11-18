42105bcc3a6b0e9febda143174ed7cce73801874
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-11-17 20:59:42 -0500

    Let's make some classrooms

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -2,6 +2,14 @@
 
 import ClassRoom from "./0-classroom.js";
 
+const room1 = new ClassRoom(19);
+const room2 = new ClassRoom(20);
+const room3 = new ClassRoom(34);
+
 export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return [
+    room1,
+    room2,
+    room3
+  ];
 }
