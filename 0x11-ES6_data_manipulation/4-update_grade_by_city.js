6c9e53fb66218656bdbe755acf443f9799a2cc8f
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-11-30 14:29:01 -0500

    data_manipulation

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,11 @@
-export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
-  return getListStudents.map((obj) => obj["grade"] = newGrades)
-  // return getListStudents.filter((obj) => obj.location === city);
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location.localeCompare(city) === 0)
+    .map((x) => {
+      const newGrade = newGrades.filter((y) => y.studentId === x.id);
+      const student = x;
+      if (newGrade.length === 1) student.grade = newGrade[0].grade;
+      else student.grade = 'N/A';
+      return student;
+    });
 }
